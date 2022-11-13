7260a2d2485f70f0dd3a331ded59b610f9bd18a7
Guscraftin <augustin.glorian@outlook.com>
2022-11-13 21:46:41 +0100

    Add X time for invite people in server
    Signed-off-by: Guscraftin <augustin.glorian@outlook.com>

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,6 @@
 const { Events } = require("discord.js");
+const { guild, channelPanelId } = require("../constVar.json");
+const cron = require("cron");
 
 module.exports = {
     name: Events.ClientReady,
@@ -6,5 +8,42 @@ module.exports = {
     execute(client) {
         client.user.setPresence({ activities: [{ name: "organiser vos soirées !", type: 0 }], status: "online" });
         console.log(`Ready! Logged in as ${client.user.tag}`);
+
+
+        const MPInvite = new cron.CronJob("00 00 21 1,7,14,20,26 * *", async function() {
+            let guildParty = 0;
+            await client.guilds.fetch(guild).then(function(guild1) {
+                guildParty = guild1;
+            });
+
+            let channelPanel;
+            await guildParty.channels.fetch(channelPanelId).then(channel => channelPanel = channel);
+            let inviteURL;
+            await guildParty.invites.create(channelPanel, { maxAge: 0, maxUses: 0, reason: "Invite des membres qui sont sur un autre serveur." }).then(function(invite) {
+                inviteURL = invite.url;
+            });
+
+            await client.guilds.fetch().then(async function(guilds) {
+                await guilds.each(async function(partGuild) {
+                    if (partGuild.id !== guild) {
+                        await partGuild.fetch().then(async function(guild1) {
+                            await guild1.members.fetch().then(async function(members) {
+                                await members.each(async function(member) {
+                                    if (await guildParty.members.fetch().then(membre => !membre.has(member.id) && !member.user.bot)) {
+                                        member.send(`👋 Salut ${member.user.username} !\n\n` +
+                                        `> Je viens te voir car __tu n'es toujours pas__ sur le serveur discord **\`${guildParty.name}\`**.\n` +
+                                        "> Ce serveur **regroupe tous les événements organisés par les personnes présentes sur les même serveurs que toi** !\n" +
+                                        `> Vient donc les rejoindre grâce à cette invitation ${inviteURL} afin que toi aussi tu puisses organiser tes soirées et être invité 🎉 !`);
+                                        console.log(`Envoie d'une invite à ${member.user.tag}`);
+                                    }
+                                });
+                            });
+                        });
+                    };
+                });
+            });
+        });
+
+        MPInvite.start();
     },
 };
