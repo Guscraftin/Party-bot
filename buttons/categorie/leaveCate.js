b8f9ce0d9e6161076a825ab855a0a55c23d1fd06
Guscraftin <augustin.glorian@outlook.com>
2022-12-30 21:22:10 +0100

    Fix leaveCaté quand pas invité
    Signed-off-by: Guscraftin <augustin.glorian@outlook.com>

--- buttons/categorie/leaveCate.js
+++ buttons/categorie/leaveCate.js
@@ -1,4 +1,4 @@
-const { isRemoveInvite, isPanelOrga } = require("../../utils/utilities");
+const { isPanelOrga } = require("../../utils/utilities");
 
 module.exports = {
     data: {
@@ -9,7 +9,7 @@ module.exports = {
         const cateId = channel.parentId;
         const membre = interaction.member;
 
-        if (await isRemoveInvite(cateId, membre.id)) {
+        try { // (await isRemoveInvite(cateId, membre.id)) // => Car parfois les gens ne sont détecté invités
             await channel.parent.permissionOverwrites.delete(membre, `Par la volonté de l'invité (${membre.id}) !`);
             await channel.parent.children.cache.each(function(channel1) {
                 channel1.permissionOverwrites.delete(membre);
@@ -20,7 +20,7 @@ module.exports = {
                     channel1.send(`<@${membre.id}> a quitté ta soirée !`);
                 }
             });
-        } else {
+        } catch (error) {
             return interaction.reply({
                 content: `<@${membre.id}> n'a pas pu être retiré de cette soirée !`,
                 ephemeral: true,
