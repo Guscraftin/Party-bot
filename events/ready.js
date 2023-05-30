bfab21d4e77f519e858a681c1f814f3f9d77ae5d
Guscraftin <97472565+Guscraftin@users.noreply.github.com>
2023-05-30 16:06:44 +0100

    fix: ready.js - send automatic message to join

--- events/ready.js
+++ events/ready.js
@@ -32,16 +32,20 @@ module.exports = {
                         await partGuild.fetch().then(async function(guild1) {
                             await guild1.members.fetch().then(async function(members) {
                                 await members.each(async function(member) {
-                                    if (await guildParty.members.fetch().then(membre => !membre.has(member.id) && !member.user.bot) && blacklistMP.find(userId => userId === member.id) === undefined) {
-                                        try {
-                                            member.send(`👋 Salut ${member.user.username} !\n\n` +
-                                            `> Je viens te voir car __tu n'es toujours pas__ sur le serveur discord **\`${guildParty.name}\`**.\n` +
-                                            "> Ce serveur **regroupe tous les événements organisés par les personnes présentes sur les même serveurs que toi** !\n" +
-                                            `> Vient donc les rejoindre grâce à cette invitation ${inviteURL} afin que toi aussi tu puisses organiser tes soirées et être invité 🎉 !`);
-                                            console.log(`Envoie d'une invite à ${member.user.tag}`);
-                                        } catch (error) {
-                                            console.log(`Impossible d'envoyer une invitation à ${member.user.tag}`);
+                                    try {
+                                        if (await guildParty.members.fetch().then(membre => !membre.has(member.id) && !member.user.bot) && blacklistMP.find(userId => userId === member.id) === undefined) {
+                                            try {
+                                                member.send(`👋 Salut ${member.user.username} !\n\n` +
+                                                `> Je viens te voir car __tu n'es toujours pas__ sur le serveur discord **\`${guildParty.name}\`**.\n` +
+                                                "> Ce serveur **regroupe tous les événements organisés par les personnes présentes sur les même serveurs que toi** !\n" +
+                                                `> Vient donc les rejoindre grâce à cette invitation ${inviteURL} afin que toi aussi tu puisses organiser tes soirées et être invité 🎉 !`);
+                                                console.log(`Envoie d'une invite à ${member.user.tag}`);
+                                            } catch (error) {
+                                                console.log(`Impossible d'envoyer une invitation à ${member.user.tag}`);
+                                            }
                                         }
+                                    } catch (error) {
+                                        console.log(`Impossible de vérifier si ${member.user.tag} est sur le serveur`);
                                     }
                                 });
                             });
